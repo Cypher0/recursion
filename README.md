6b649547a1a20dcaebd200d726248a2079e7ebdd
Raiko <raiko4238@gmail.com>
2017-01-20 00:06:52 +0200

    completed warmup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # recursion
 Solution for Project: Recursion for The Odin Project
+
+http://www.theodinproject.com/courses/ruby-programming/lessons/recursion?ref=lnav
---
+++ fibs.rb
@@ -0,0 +1,19 @@
+def fibs(num)
+  result = [0, 1]
+  if num.zero?
+    nil
+  elsif num == 1
+    [0]
+  else
+    (num - 2).times do
+      result << result[-1] + result[-2]
+    end
+    result
+  end
+end
+
+def fibs_rec(num, result = [0, 1])
+  return result if num == 2
+  result << result[-1] + result[-2]
+  fibs_rec(num - 1, result)
+end
